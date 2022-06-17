45925857bfbd20c0f2f437589e41c62f703273a5
aurel0099 <106863679+aurel0099@users.noreply.github.com>
2022-06-17 13:27:11 +0300

    Update schemas.js

--- utils/schemas.js
+++ utils/schemas.js
@@ -4,6 +4,9 @@ const User = new mongoose.Schema({
     id: { type: String, unique: true, required: true },
     wallet: { type: Number, default: 0 },
     bank: { type: Number, default: 0 }
+    cooldowns: {
+     work: { type: Date }
+    }
 })
 
 module.exports = { User: mongoose.model("User", User) }
